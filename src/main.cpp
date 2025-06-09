5a88c0ae8e360ffd8201616fbadc090f8a9ffd6a
yv0503 <yvanstephen0503@gmail.com>
2025-06-09 14:05:19 +0800

    Adjusted Display Reset

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
+#include <ThreeWire.h>
+#include <RtcDS1302.h>
 
 Servo myServo;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
+ThreeWire rtcWire(6, 7, 8);
+RtcDS1302<ThreeWire> Rtc(rtcWire);
 
 constexpr int redButtonPin = 2;
 constexpr int blueButtonPin = 4;
@@ -13,6 +17,7 @@ int redButtonState = 0;
 int blueButtonState = 0;
 int turnTimes = 0;
 bool pressedState = false;
+bool displayNotResetted = false;
 
 int centerAlign(const String &textToPrint) {
   const int centerPoint = (20 - textToPrint.length()) / 2;
@@ -32,7 +37,6 @@ void lcdDisplayReset() {
 }
 
 void lcdChangeText(const String &string1, const String &string2, const String &string3, const String &string4) {
-  lcdDisplayReset();
   lcd.setCursor(centerAlign(string1), 0);    // move cursor the first row and center align
   lcd.print(string1);                        // print message at the first row
   lcd.setCursor(centerAlign(string2), 1);    // move cursor to the second row and center align
@@ -55,33 +59,51 @@ int servoTurn(int turnTimes, const int timeForRotateToNextContainer) {
 }
 
 void servoReset(const int number, const int timeForRotateToNextContainer) {
+  lcdDisplayReset();
     switch (number) {
       case 1:
-        lcdChangeText("LCD ","Blue Button", "Has been", "Pressed 1");
+        lcdChangeText("LCD","Blue Button", "Has been", "Pressed 1");
         myServo.write(180);
         delay(timeForRotateToNextContainer * 4);
         myServo.write(90);
         break;
       case 2:
-        lcdChangeText("LCD ","Blue Button", "Has been", "Pressed 2");
+        lcdChangeText("LCD","Blue Button", "Has been", "Pressed 2");
         myServo.write(180);
         delay(timeForRotateToNextContainer * 3);
         myServo.write(90);
         break;
       case 3:
-        lcdChangeText("LCD ","Blue Button", "Has been", "Pressed 3");
+        lcdChangeText("LCD","Blue Button", "Has been", "Pressed 3");
         myServo.write(180);
         delay(timeForRotateToNextContainer * 2);
         myServo.write(90);
         break;
       case 4:
-        lcdChangeText("LCD ","Blue Button", "Has been", "Pressed 4");
+        lcdChangeText("LCD","Blue Button", "Has been", "Pressed 4");
         myServo.write(180);
         delay(timeForRotateToNextContainer);
         myServo.write(90);
         break;
-      default: ;
+      default:
+        lcdChangeText("LCD","Blue Button", "Has been", "Pressed 1");
     }
+}
+
+String DateTime(const RtcDateTime& dt)
+{
+  char datestring[26];
+
+  snprintf_P(datestring,
+          countof(datestring),
+          PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+          dt.Month(),
+          dt.Day(),
+          dt.Year(),
+          dt.Hour(),
+          dt.Minute(),
+          dt.Second() );
+  return datestring;
 }
 
 void setup() {
@@ -90,29 +112,41 @@ void setup() {
   lcd.init();
   lcd.backlight();
   myServo.attach(9);
+  Rtc.Begin();
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  lcdChangeText("PharmAssistV1", "The Auto Dispenser", __DATE__, __TIME__); // Intro text
+  delay(2000);
+  lcdDisplayReset();
 }
 
 void loop() {
+  RtcDateTime now = Rtc.GetDateTime();
   redButtonState = digitalRead(redButtonPin);
   blueButtonState = digitalRead(blueButtonPin);
 
   if (redButtonState == HIGH && pressedState == false) {
+    lcdDisplayReset();
     lcdChangeText("LCD ","Red Button", "Has been", "Pressed");
     turnTimes = servoTurn(turnTimes, timeForRotateToNextContainer);
     pressedState = true;
+    displayNotResetted = true;
   }
 
-  if (blueButtonState == HIGH && pressedState == false) {
+  else if (blueButtonState == HIGH && pressedState == false) {
     servoReset(turnTimes, timeForRotateToNextContainer);
     turnTimes = 0;
     pressedState = true;
+    displayNotResetted = true;
   }
 
-  else if (blueButtonState == LOW && redButtonState == LOW && pressedState == true) {
-    lcdChangeText("LCD ","Buttons", "are", "Available");
+  else if (blueButtonState == LOW && redButtonState == LOW) {
+    if (displayNotResetted == true) {
+      lcdDisplayReset();
+      displayNotResetted = false;
+    }
+    lcdChangeText("Buttons","Available", "", DateTime(now));
     pressedState = false;
   }
-
 
 }
 
